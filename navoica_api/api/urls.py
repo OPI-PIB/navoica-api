b94442a36a6139ac2ed3aee658977a332b7f7958
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2021-05-02 18:18:29 +0200

    MOOC-1797 Feat: set sitemap url

--- navoica_api/api/urls.py
+++ navoica_api/api/urls.py
@@ -5,9 +5,14 @@ Api URLs.
 from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import include, url
+from django.contrib.sitemaps.views import sitemap
+
+from navoica_api.sitemaps import CourseOverviewSitemap
 
 app_name = 'navoica_api'
 
 urlpatterns = [
     url(r'^v1/', include('navoica_api.api.v1.urls')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': {'courses': CourseOverviewSitemap()}},
+        name='django.contrib.sitemaps.views.sitemap'),
 ]
