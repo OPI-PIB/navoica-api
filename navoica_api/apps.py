b600703f11e59c2e3442caf9547704e96b99aba1
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2021-10-18 15:36:16 +0200

    Fix: generate certificate only for object where download_url is missing and status is downloadable

--- navoica_api/apps.py
+++ navoica_api/apps.py
@@ -2,9 +2,7 @@
 Config of navoica API app
 '''
 from __future__ import unicode_literals
-
 from django.apps import AppConfig
-# from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType, PluginURLs, PluginSettings
 
 
 class NavoicaApiConfig(AppConfig):
@@ -14,8 +12,7 @@ class NavoicaApiConfig(AppConfig):
     name = u'navoica_api'
 
     def ready(self):
-        from navoica_api.certificates.signals import handle_generating_cert_awarded
-        handle_generating_cert_awarded #signal need to be imported after module is ready instead signal wont works
+        pass
 
     # plugin_app = {
     #     PluginURLs.CONFIG: {
