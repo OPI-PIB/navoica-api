fb994c23ddd8b0e7724ea0b509ad0b38b599bf73
krystianlygas <51002868+krystianlygas@users.noreply.github.com>
2022-03-08 09:40:58 +0100

    MOOC-2821: add career model, create readonlyviewset and option of data ajustment in django admin (#21)

--- navoica_api/models.py
+++ navoica_api/models.py
@@ -40,3 +40,42 @@ class CourseRunOpinionModel(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['course_id', 'user'], name='constraint')
         ]
+
+
+class CareerModel(models.Model):
+    """
+    Model for Career Site
+    """
+    FULL = 'FL'
+    DOUBLE = 'DB'
+    TRIPLE = 'TR'
+    HALF = 'HF'
+    QUARTER = 'QR'
+    EMPLOYMENT_TYPE_CHOICES = [
+        (FULL, '1 etat'),
+        (DOUBLE, '2 etaty'),
+        (TRIPLE, '3 etaty'),
+        (HALF, '1/2 etatu'),
+        (QUARTER, '1/4 etatu'),
+    ]
+
+    job_title = models.CharField(max_length=255)
+    preface = models.CharField(max_length=1000)
+    employment_size = models.CharField(
+        max_length=2,
+        choices=EMPLOYMENT_TYPE_CHOICES,
+        default=FULL,
+    )
+    localization = models.CharField(max_length=255)
+    offer = models.CharField(max_length=1000)
+    responsibilities = models.CharField(max_length=1000)
+    expectations = models.CharField(max_length=1000)
+    welcomed = models.CharField(max_length=1000)
+    attachment = models.CharField(max_length=3000)
+    link = models.URLField()
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    publish = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.job_title
