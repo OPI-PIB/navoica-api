3eb0e608ac96af590fb4b95f165e8d2756a53de7
developer <edxapp@plisi-dev7.novalocal>
2023-10-23 11:39:19 +0000

    changes

--- navoica_api/api/v1/branding/urls.py
+++ navoica_api/api/v1/branding/urls.py
@@ -2,8 +2,8 @@
 Branding API endpoint urls.
 """
 from django.conf.urls import url
-from navoica_api.api.v1.branding.views import navoica_footer
+# from navoica_api.api.v1.branding.views import navoica_footer
 
 BRANDING_URLS = [
-    url(r"^footer$", navoica_footer, name="branding_footer"),
+    url(r"^footer$", "branding_footer"),
 ]
